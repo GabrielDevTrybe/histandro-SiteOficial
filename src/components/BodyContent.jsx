489c5d5efea1322fc7d6ca83b081d4babd20a2a7
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-03-24 23:58:46 -0300

    adicionei imagem da logo

--- src/components/BodyContent.jsx
+++ src/components/BodyContent.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Logo from '../images/histandro.jpeg'
 
 function BodyContent() {
   return (
-    <div className='BodyContent'>Te amoooooo meu amor</div>
+    <>
+      <div className='Logo'>
+      <img className='img-logo' src={Logo} alt="histandro" />
+      </div>
+      <div className='BodyContent'>
+        <p className='description-content'>
+          Para o público que gosta de gêneros como:<br></br> terror, fantasia, suspense, drama, aventura e romance
+          Garanto a vocês, grandes emoções      </p>
+      </div>
+    </>
   )
 }
 
