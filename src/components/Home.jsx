b161abb963ee1cb1492162a51548aa03a0ea07b1
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-05-24 15:53:26 -0300

    corrigindo cor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -49,7 +49,7 @@ function Home() {
       <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
     </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul id='cor-a' className="font-medium flex flex-col p-4 md:p-0 mt-4 border-2 border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+      <ul  className="font-medium flex flex-col p-4 md:p-0 mt-4 border-2 border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
         <li>
           <a href="#" className="block py-2 pl-3 pr-4 text-black-900 rounded md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0" aria-current="page">Home</a>
         </li>
